8558a53259302d4c7984ae8227e3dfce8d43bd07
StefanKehayov <stefan.kehayov.96@gmail.com>
2021-06-10 19:10:33 +0300

    Commit Main

---
+++ awesome_application/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/stefankehayov/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Cursed/Current Projects/Learning_Flutter/awesome_application"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Volumes/Cursed/Current Projects/Learning_Flutter/awesome_application/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Volumes/Cursed/Current Projects/Learning_Flutter/awesome_application/.dart_tool/package_config.json"
